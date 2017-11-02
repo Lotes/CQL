c684f2d72158aec16496d1abb95d1b91de846094
Lotes <markhoernchen@gmx.de>
2017-11-02 14:31:12 +0100

    add switchable editor

---
+++ MainCore.CQL.WPF/Editor.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MainCore.CQL.SyntaxTree;
+using MainCore.CQL.Contexts;
+
+namespace MainCore.CQL.WPF
+{
+    /// <summary>
+    /// Interaktionslogik für Editor.xaml
+    /// </summary>
+    public partial class Editor : UserControl
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsSimple
+        {
+            get { return (bool)GetValue(IsSimpleProperty); }
+            set { SetValue(IsSimpleProperty, value); }
+        }
+        public static readonly DependencyProperty IsSimpleProperty =
+            DependencyProperty.Register("IsSimple", typeof(bool), typeof(Editor), new PropertyMetadata(true));
+
+        public bool IsSimplifyable
+        {
+            get { return (bool)GetValue(IsSimplifyablePropertyKey.DependencyProperty); }
+            private set { SetValue(IsSimplifyablePropertyKey, value); }
+        }
+        public static readonly DependencyPropertyKey IsSimplifyablePropertyKey =
+            DependencyProperty.RegisterReadOnly("IsSimplifyable", typeof(bool), typeof(Editor), new PropertyMetadata(true));
+
+        public IContext Context
+        {
+            get { return (IContext)GetValue(ContextProperty); }
+            set { SetValue(ContextProperty, value); }
+        }
+        public static readonly DependencyProperty ContextProperty =
+            DependencyProperty.Register("Context", typeof(IContext), typeof(Editor), new PropertyMetadata(null));
+        
+        public Query Query
+        {
+            get { return (Query)GetValue(QueryProperty); }
+            set { SetValue(QueryProperty, value); }
+        }
+        public static readonly DependencyProperty QueryProperty =
+            DependencyProperty.Register("Query", typeof(Query), typeof(Editor), new PropertyMetadata(null));
+    }
+}
