ce3ca1522fb7789ce9e0f5102eae661271b3125f
Lotes <markhoernchen@gmx.de>
2018-03-10 20:14:13 +0100

    update nuget specs

--- packaging/BuildNugetPackage.bat
+++ packaging/BuildNugetPackage.bat
@@ -1,7 +1,7 @@
 @if exist "%ProgramFiles%\MSBuild\14.0\bin" set PATH=%ProgramFiles%\MSBuild\14.0\bin;%PATH%
 @if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin" set PATH=%ProgramFiles(x86)%\MSBuild\14.0\bin;%PATH%
 
-@msbuild %~dp0..\..\CQL.sln /p:Configuration=Release
+@msbuild %~dp0..\CQL.sln /p:Configuration=Release
 @nuget pack CQL.nuspec
 
 @echo NOW PUSH WITH:
